a8b204cf03992c3da6f8d8b7bd7f7b8e731e747d
Ba-jonzz <bahaj@hello-jonzz>
2024-02-22 12:57:51 +0100

    init project

---
+++ src/App.js
@@ -0,0 +1,93 @@
+import React, { useEffect ,useMemo,useState} from 'react';
+import "./App.css";
+import NetworkGraph from './NetworkGraph';
+const MyApp = () => {
+  return (
+    <div className="App">
+      <NetworkGraph data={dataElements} onHoverLine={(event) => (<>hello</>)} onHoverNode={(event) => (<>Node</>)}  />
+    </div>
+  );
+}
+
+export default MyApp;
+
+
+const colorNode = {
+  primary:{
+    bg:"#E7EAEE",
+    border:"#919AAB"
+  },
+  secondary:{
+    bg:"#D1E1FF",
+    border:"#2F80ED"
+  }
+}
+
+const colorEdge = {
+  green:"#44DA86",
+  black:"#000"
+}
+
+
+const sizeNode = {
+  sm:15,
+  md:22,
+  lg:25,
+  xl:38
+}
+
+const sizeEdge = {
+  sm:0.5,
+  md:1,
+  lg:1.5,
+}
+
+
+
+
+const dataElements = [
+  { data: { id: 'one', label: "Node 1" , title: "other 1", fullname: "John Smith" ,options:{bgColor:colorNode.secondary.bg,borderColor:colorNode.secondary.border, size:sizeNode.xl}}},
+  { data: { id: 'two', label: 'Node 2', title: "other 2", fullname: "John Smith",options:{bgColor:colorNode.secondary.bg,borderColor:colorNode.secondary.border, size:sizeNode.lg}}},
+  { data: { id: 'three', label: 'Node 3' , title: "other 3", fullname: "John Smith",options:{bgColor:colorNode.secondary.bg,borderColor:colorNode.secondary.border, size:sizeNode.lg}} },
+  { data: { id: 'four', label: 'Node 4' , title: "other 4", fullname: "John Smith",options:{bgColor:colorNode.secondary.bg,borderColor:colorNode.secondary.border, size:sizeNode.lg}} },
+  { data: { id: 'five', label: 'Node 5' , title: "other 5", fullname: "John Smith",options:{bgColor:colorNode.secondary.bg,borderColor:colorNode.secondary.border, size:sizeNode.lg}} },
+  { data: { id: 'six', label: 'Node 6' , title: "other 6", fullname: "John Smith",options:{bgColor:colorNode.primary.bg,borderColor:colorNode.primary.border, size:sizeNode.lg}}},
+  { data: { id: 'seven', label: 'Node 7' , title: "other 7", fullname: "John Smith",options:{bgColor:colorNode.primary.bg,borderColor:colorNode.primary.border, size:sizeNode.lg}}},
+  { data: { id: 'eight', label: 'Node 8' , title: "other 8", fullname: "John Smith",options:{bgColor:colorNode.primary.bg,borderColor:colorNode.primary.border, size:sizeNode.md}} },
+  { data: { id: 'nine', label: 'Node 9' , title: "other 9", fullname: "John Smith",options:{bgColor:colorNode.primary.bg,borderColor:colorNode.primary.border, size:sizeNode.md}}},
+  { data: { id: 'ten', label: 'Node 10' , title: "other 10", fullname: "John Smith",options:{bgColor:colorNode.primary.bg,borderColor:colorNode.primary.border, size:sizeNode.md}} },
+  { data: { id: 'eleven', label: 'Node 11' , title: "other 11", fullname: "John Smith",options:{bgColor:colorNode.primary.bg,borderColor:colorNode.primary.border, size:sizeNode.md}}},
+  { data: { id: 'twelve', label: 'Node 12' , title: "other 12", fullname: "John Smith",options:{bgColor:colorNode.primary.bg,borderColor:colorNode.primary.border, size:sizeNode.md}}},
+  { data: { id:'thirteen',label:'Node 13', title: "other 13", fullname: "John Smith",options:{bgColor:colorNode.primary.bg,borderColor:colorNode.primary.border, size:sizeNode.md}}},
+  { data: { id:'fourteen',label:'Node 14', title: "other 14", fullname: "John Smith",options:{bgColor:colorNode.primary.bg,borderColor:colorNode.primary.border, size:sizeNode.sm}}},
+  { data: { id:'fifteen',label:'Node 15', title: "other 15", fullname: "John Smith",options:{bgColor:colorNode.primary.bg,borderColor:colorNode.primary.border, size:sizeNode.sm}}},
+  { data: { id:'sixteen',label:'Node 16', title: "other 16", fullname: "John Smith",options:{bgColor:colorNode.primary.bg,borderColor:colorNode.primary.border, size:sizeNode.sm}}},
+  { data: { id:'seventeen',label:'Node 17', title: "other 17", fullname: "John Smith",options:{bgColor:colorNode.primary.bg,borderColor:colorNode.primary.border, size:sizeNode.sm}}},
+  { data: { id:'eighteen',label:'Node 18', title: "other 18", fullname: "John Smith",options:{bgColor:colorNode.primary.bg,borderColor:colorNode.primary.border, size:sizeNode.sm}}},
+  { data: { id:'nineteen',label:'Node 19', title: "other 19", fullname: "John Smith",options:{bgColor:colorNode.primary.bg,borderColor:colorNode.primary.border, size:sizeNode.sm}}},
+  { data: { id:'twenty',title: "other 20", fullname: "John Smith",options:{bgColor:colorNode.primary.bg,borderColor:colorNode.primary.border, size:sizeNode.sm}}},
+
+  { data: {  id: '1', source: 'one', target: 'two', label: 'Edge from Node1 to Node2' ,size:  sizeEdge.sm, color:colorEdge.black }},
+  { data: {  id: '2',source: 'two', target: 'three', label: 'Edge from Node2 to Node3' ,size:sizeEdge.sm ,color:colorEdge.black }},
+  { data: {  id: '3',source: 'three', target: 'four', label: 'Edge from Node3 to Node4' ,size: sizeEdge.md,color:colorEdge.black }},
+  { data: {  id: '4',source: 'four', target: 'five', label: 'Edge from Node4 to Node5' ,size:sizeEdge.md ,color:colorEdge.black }},
+  { data: {  id: '5',source: 'five', target: 'six', label: 'Edge from Node5 to Node6' ,size: sizeEdge.lg ,color:colorEdge.black }},
+  { data: {  id: '6',source: 'six', target: 'seven', label: 'Edge from Node6 to Node7' ,size: sizeEdge.lg,color:colorEdge.black }},
+  { data: {  id: '7',source: 'seven', target: 'eight', label: 'Edge from Node7 to Node8' ,size: sizeEdge.sm,color:colorEdge.green }},
+  { data: {  id: '8',source: 'eight', target: 'nine', label: 'Edge from Node8 to Node9' ,size: sizeEdge.sm,color:colorEdge.green }},
+  { data: {  id: '9',source: 'nine', target: 'ten', label: 'Edge from Node9 to Node10' ,size: sizeEdge.md,color:colorEdge.green }},
+  { data: {  id: '10',source: 'ten', target: 'eleven', label: 'Edge from Node10 to Node11' ,size: sizeEdge.md,color:colorEdge.green }},
+  { data: {  id: '11',source: 'eleven', target: 'twelve', label: 'Edge from Node11 to Node12' ,size: sizeEdge.lg,color:colorEdge.green }},
+  { data: {  id: '13',source: 'one', target: 'four', label: 'Edge from Node12 to Node1' ,size: sizeEdge.lg,color:colorEdge.green}},
+  { data: {  id: '14',source: 'four', target: 'seven', label: 'Edge from Node13 to Node4' ,size: sizeEdge.lg,color:colorEdge.green}},
+  { data: {  id: '15',source: 'seven', target: 'ten', label: 'Edge from Node14 to Node7' ,size: sizeEdge.lg,color:colorEdge.green}},
+  { data: {  id: '16',source: 'ten', target: 'thirteen', label: 'Edge from Node15 to Node10' ,size: sizeEdge.lg,color:colorEdge.green}},
+  { data: {  id: '17',source: 'thirteen', target: 'sixteen', label: 'Edge from Node16 to Node13' ,size: sizeEdge.lg,color:colorEdge.green}},
+  { data: {  id: '18',source: 'sixteen', target: 'nineteen', label: 'Edge from Node17 to Node16' ,size: sizeEdge.lg,color:colorEdge.green}},
+  { data: {  id: '19',source: 'nineteen', target: 'twenty', label: 'Edge from Node18 to Node19' ,size: sizeEdge.lg,color:colorEdge.green}},
+  { data: {  id: '20',source: 'twenty', target: 'one', label: 'Edge from Node19 to Node20' ,size: sizeEdge.lg,color:colorEdge.green}},
+  
+];
+
+
+
---
+++ src/NetworkGraph.js
@@ -0,0 +1,144 @@
+import React, { useEffect, useMemo, useState } from 'react';
+import Cytoscape from 'cytoscape';
+
+const NetworkGraph = ({ data,onHoverLine,onHoverNode }) => {
+  const [contentPopup, setContentPopup] = useState(null);
+  const [graphNetwork, setGraphNetwork] = useState(null);
+
+  const handleNodeMouseover = event => {
+    if (!!onHoverNode) {
+    const position = event.renderedPosition
+    const customContent =  onHoverNode(event);
+    const content = (<ContainerTooltip style={{top:position.y,left:position.x}}>{customContent}</ContainerTooltip>)
+    setContentPopup(content);
+    }
+  };
+
+  const handleEdgeMouseover = event => {    
+    if (!!onHoverLine) {
+      const position = event.renderedPosition
+      const customContent =  onHoverLine(event);
+      const content = (<ContainerTooltip style={{top:position.y,left:position.x}}>{customContent}</ContainerTooltip>)
+      setContentPopup(content);
+    }
+
+  };
+
+  const handleMouseout = event => {
+    setContentPopup(null);
+  };
+
+  const positions = useMemo(() => [
+    { x: 481, y: 118 },
+    { x: 640, y: 126 },
+    { x: 587, y: 205 },
+    { x: 403, y: 273 },
+    { x: 396, y: 165 },
+    { x: 726, y: 170 },
+    { x: 654, y: 238 },
+    { x: 504, y: 348 },
+    { x: 314, y: 228 },
+    { x: 337, y: 94 },
+    { x: 386, y: 28 },
+    { x: 624, y: 317 },
+    { x: 835, y: 238 },
+    { x: 749, y: 305 },
+    { x: 572, y: 398 },
+    { x: 226, y: 245 },
+    { x: 267, y: 75 },
+    { x: 753, y: 390 },
+    { x: 664, y: 442 },
+    { x: 198, y: 149 },
+  ], []);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && data?.length > 0) {
+      const elements = data?.map((element, index) => {
+        return element.data.source
+          ? element
+          : {
+              ...element,
+              position: positions[index],
+            };
+      });
+
+      const cyInstance = Cytoscape({
+        elements,
+        container: document.getElementById('graph-network'),
+        style: stylesheet,
+        layout: { name: 'preset' },
+        minZoom: 1,
+        maxZoom: 2,
+        userPanningEnabled: false,
+      });
+
+      cyInstance.on('mouseover', 'node', handleNodeMouseover);
+      cyInstance.on('mouseover', 'edge', handleEdgeMouseover);
+      cyInstance.on('mouseout', 'node,edge', handleMouseout);
+
+      setGraphNetwork(cyInstance);
+      
+      return () => {
+        cyInstance?.destroy();
+        setGraphNetwork(null)
+      };
+    }
+  }, [data]);
+
+  const handleZoom = factor => {
+    if (graphNetwork) {
+      graphNetwork.zoom(graphNetwork.zoom() * factor);
+    }
+  };
+
+  return (
+    <div className='container-graph-network'>
+      <div id="graph-network" className='graph-network'/>
+      <div className='container-control-zoom'>
+      <button onClick={() => handleZoom(1.1)}>+</button>
+      <button onClick={() => handleZoom(0.9)}>-</button>
+      </div>
+      
+      {contentPopup}
+    </div>
+  );
+};
+
+const stylesheet = [
+  {
+    selector: 'edge',
+    style: {
+      'curve-style': 'bezier',
+      'target-arrow-shape': 'triangle',
+      'target-arrow-color': 'data(color)',
+      'line-color': 'data(color)',
+      width: 'data(size)',
+    },
+  },
+  {
+    selector: 'node',
+    style: {
+      'background-color': 'data(options.bgColor)',
+      'border-color': 'data(options.borderColor)',
+      'border-width': 0.8,
+      width: 'data(options.size)',
+      height: 'data(options.size)',
+      'border-style': 'solid',
+      label: 'data(label)',
+      'font-size': '13px',
+      color: '#5A88FF'    },
+  },
+];
+
+
+const  ContainerTooltip = ({children,...props}) => {
+  return (
+    <div className='container-tooltip' {...props}>
+       <div className="content-tooltip" >
+        {children}
+       </div>
+    </div>
+  )
+}
+
+export default NetworkGraph;
